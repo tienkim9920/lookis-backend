c70cc2a3003ec321225eb1a92713811737934de6
Tien Kim <tienkim9920@gmail.com>
2023-11-05 20:22:34 +0700

    update

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -22,57 +22,57 @@ export class ProductController {
     }
   }
 
-  @Public()
-  @Post()
-  createProduct(@Body() product: ProductDto, @Res() res: Response): ResponseType<Product> {
-    try {
-      return res.json(new ResponseData(this.productService.createProduct(product), ServerStatus.OK, ServerMessage.OK));
-    } catch (error) {
-      return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
-    }
-  }
+  // @Public()
+  // @Post()
+  // createProduct(@Body() product: ProductDto, @Res() res: Response): ResponseType<Product> {
+  //   try {
+  //     return res.json(new ResponseData(this.productService.createProduct(product), ServerStatus.OK, ServerMessage.OK));
+  //   } catch (error) {
+  //     return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
+  //   }
+  // }
 
-  @Public()
-  @Get('/:id')
-  detailProduct(@Param('id') id: number, @Res() res: Response): ResponseType<Product> {
-    try {
-      return res.json(new ResponseData(this.productService.findById(id), ServerStatus.OK, ServerMessage.OK));
-    } catch (error) {
-      return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
-    }
-  }
+  // @Public()
+  // @Get('/:id')
+  // detailProduct(@Param('id') id: number, @Res() res: Response): ResponseType<Product> {
+  //   try {
+  //     return res.json(new ResponseData(this.productService.findById(id), ServerStatus.OK, ServerMessage.OK));
+  //   } catch (error) {
+  //     return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
+  //   }
+  // }
 
-  @Public()
-  @Put('/:id')
-  updateProduct(@Param('id') id: number, @Body() product: ProductDto, @Res() res: Response): ResponseType<Product> {
-    try {
-      return res.json(new ResponseData(this.productService.updateProduct(id, product), ServerStatus.OK, ServerMessage.OK));
-    } catch (error) {
-      return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
-    }
-  }
+  // @Public()
+  // @Put('/:id')
+  // updateProduct(@Param('id') id: number, @Body() product: ProductDto, @Res() res: Response): ResponseType<Product> {
+  //   try {
+  //     return res.json(new ResponseData(this.productService.updateProduct(id, product), ServerStatus.OK, ServerMessage.OK));
+  //   } catch (error) {
+  //     return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
+  //   }
+  // }
 
-  @Public()
-  @Delete('/:id')
-  deleteProduct(@Param('id') id: number, @Res() res: Response): ResponseType<Product> {
-    try {
-      return res.json(new ResponseData(this.productService.deleteProduct(id), ServerStatus.OK, ServerMessage.OK));
-    } catch (error) {
-      return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
-    }
-  }
+  // @Public()
+  // @Delete('/:id')
+  // deleteProduct(@Param('id') id: number, @Res() res: Response): ResponseType<Product> {
+  //   try {
+  //     return res.json(new ResponseData(this.productService.deleteProduct(id), ServerStatus.OK, ServerMessage.OK));
+  //   } catch (error) {
+  //     return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
+  //   }
+  // }
 
-  @Public()
-  @Get('/home/pagination')
-  getHomeProducts(
-    @Query('page') page: string,
-    @Query('search') search: string,
-    @Res() res: Response,
-  ): ResponseType<Product> {
-    try {
-      return res.json(new ResponseData(this.productService.findProductHome({ page, search }), ServerStatus.OK, ServerMessage.OK));
-    } catch (error) {
-      return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
-    }
-  }
+  // @Public()
+  // @Get('/home/pagination')
+  // getHomeProducts(
+  //   @Query('page') page: string,
+  //   @Query('search') search: string,
+  //   @Res() res: Response,
+  // ): ResponseType<Product> {
+  //   try {
+  //     return res.json(new ResponseData(this.productService.findProductHome({ page, search }), ServerStatus.OK, ServerMessage.OK));
+  //   } catch (error) {
+  //     return res.json(new ResponseData(null, ServerStatus.ERROR, ServerMessage.ERROR));
+  //   }
+  // }
 }
